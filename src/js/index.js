43603c72fe73b45754184686486e4e5edd4f2d81
Heather Conley <hconley6695@gmail.com>
2016-03-10 22:55:11 -0500

    updated more scss for aside bar, adjusted html, and tried two js functions

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,92 @@
 // Javascript Entry Point
 import items from './items';
+
+//did not inclue hover items in this declaration
+
+//var itemHTML = `
+//	<div class="individual_item">
+ // 			<div class="photo">
+ // 				<img src="${items[0].image}"> 
+  //			</div>
+//  			<div class="item_name">${items[0].title}></div>
+//  			<div class="company">< ${items[0].shop.shop_name}</div> 
+//  			<div class="price">${items[0].price}</div><div class="currency">${items[0].currency_code}</div>
+//
+//  			<div class="disappearing_img">
+//  			</div>
+//  		</div>`
+
+
+//var picture = `<div class="photo">
+  //					<img src="${items[0].image}"> 
+  	//			</div>`
+
+
+//  $('main').append(picture);
+
+//items.forEach(function(product) {
+ //	$('.photo').append(picture);
+// 	return picture;
+//});
+
+//FIRST REAL ATTEMPT//
+var imgUrl = items.results[0].Images[0].url_170x135;
+
+var itemName = items.results[0].Shop.title;
+
+var itemCurrency = items.results[0].Shop.currency_code;
+
+var itemStore = items.results[0].Shop.shop_name;
+
+var itemPrice = items.results[0].price;
+
+var itemHTML = `<div class="individual_item">
+  			<div class="photo">
+  				<img src="${imgUrl}" alt="whiskey product"> 
+  			</div>
+  			<div class="item_name"><${itemName}</div>
+  			<div class="company">${itemStore}</div>
+  			<div class="price">${itemPrice}</div><div class="currency">${itemCurrency}</div>
+  			<div class="disappearing_img">
+  			</div>
+  		</div>`
+
+items.forEach(function(product){
+	$('main').append(itemHTML);
+});
+
+
+//SECOND REAL ATTEMPT
+
+var imgUrl = items.results[0].Images[0].url_170x135;
+
+var itemName = items.results[0].Shop.title;
+
+var itemCurrency = items.results[0].Shop.currency_code;
+
+var itemStore = items.results[0].Shop.shop_name;
+
+var itemPrice = items.results[0].price;
+
+function makeBox(product) {
+	var itemHTML = `<div class="individual_item">
+  			<div class="photo">
+  				<img src="${imgUrl}" alt="whiskey product"> 
+  			</div>
+  			<div class="item_name"><${itemName}</div>
+  			<div class="company">${itemStore}</div>
+  			<div class="price">${itemPrice}</div><div class="currency">${itemCurrency}</div>
+  			<div class="disappearing_img">
+  			</div>
+  		</div>`
+}
+
+
+
+
+
+
+
+
+
+
